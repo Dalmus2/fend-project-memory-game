25dfd0b5ec50d6c6211c80aa7d43a9b9b066736a
Dalma Szabo <k.dalma.szabo@gmail.com>
2018-03-18 20:48:52 +0100

    Set up event listener for a card

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
-
- //Create a list that holds all of your cards
+//Create a list that holds all of your cards
 
 const initialCardList = ["fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-leaf", "fa-bicycle", "fa-bomb"];
-const gameDifficulty = 2;
+const gameDifficulty = 1;
+let gameCardList = multiplyList(initialCardList, gameDifficulty);
 
 //Multiply initial list
-
 function multiplyList(initialArray, counter) {
   let multipliedArray = [];
   while (counter +1 > 0) {
@@ -15,15 +14,21 @@ function multiplyList(initialArray, counter) {
   return multipliedArray;
 }
 
+//Defining variables
+const gameDeck = document.querySelector('.deck');
+
 //Loop through each card and create its HTML
 //Add each card's HTML to the page
-
 function createCards(gameCardList) {
-  const gameDeck = document.querySelector('.deck');
   gameDeck.innerHTML = [];
   for (let i = 0; i < gameCardList.length; i++) {
     const newCard = document.createElement('li');
     newCard.classList.add('card');
+    newCard.setAttribute('value', gameCardList[i])
+    // Set up the event listener for a card.
+    newCard.addEventListener('click', function(event){
+      handleCards(event.target);
+    });
     const icon = document.createElement('i');
     icon.classList.add('fa', gameCardList[i]);
     newCard.appendChild(icon);
@@ -32,11 +37,9 @@ function createCards(gameCardList) {
   document.body.appendChild(gameDeck);
 }
 
-// Shuffle function from http://stackoverflow.com/a/2450976
-
+//Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
-
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
@@ -44,13 +47,10 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
     return array;
 }
 
 function setupGame() {
-//Define game list
-  let gameCardList = multiplyList(initialCardList, gameDifficulty);
 //Shuffle the list of cards using the provided "shuffle" method below
   gameCardList = shuffle(gameCardList);
 //Display cards on the page
@@ -58,14 +58,3 @@ function setupGame() {
 }
 
 setupGame(gameDifficulty);
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
